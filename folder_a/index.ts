989b2e166dcd07a16fe33e71944b364e915c8f0b
Juan Campuzano <diegocampuzano62@gmail.com>
2026-01-22 11:58:56 -0500

    fix: fix node cache

--- folder_a/index.ts
+++ folder_a/index.ts
@@ -6,5 +6,5 @@
 export const name = 'folder_a';
 
 export function greet(): string {
-  return `Hello from ${name}! this is a test! another test another test tesa`;
+  return `Hello from ${name}! th is is a test! another test another test tesa`;
 }
